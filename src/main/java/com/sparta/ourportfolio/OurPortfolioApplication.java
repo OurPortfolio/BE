e73c0ae1bb0e7ac4c9a0543af6192d81ef7567d2
park zihoon <bbakzi417@gmail.com>
2023-05-23 19:01:11 +0900

    Initial commit

---
+++ src/main/java/com/sparta/ourportfolio/OurPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.ourportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurPortfolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OurPortfolioApplication.class, args);
+    }
+
+}
