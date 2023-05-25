f7a18cb76b8b6af6a7bd4d6b53614abdadaf09f9
kmg0485 <kmg0485@naver.com>
2023-05-25 10:57:38 +0900

    Feat: 정규표현식 수정

--- src/main/java/com/sparta/ourportfolio/user/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/ourportfolio/user/dto/SignupRequestDto.java
@@ -16,11 +16,12 @@ public class SignupRequestDto {
     private String email;
 
     @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*?&()_])[A-Za-z\\d$@$!%*?&()_]{8,15}$",
-            message = "비밀번호는 최소 8자 이상 15자 이하이며 알파벳 대소문자와 숫자로 구성되어야 합니다.")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)[a-zA-Z\\d]{6,}$",
+            message = "비밀번호는 최소 6글자 이상이며 알파벳 대소문자와 숫자로 구성되어야 합니다.")
     private String  password;
 
     @NotBlank(message = "닉네임은 필수 입력 값입니다.")
+    @Pattern(regexp = "^[a-zA-Z가-힣0-9]{1,10}$", message = "닉네임은 10글자 이하이며, 영어 또는 한글, 숫자로 구성되어야 합니다. ")
     private String nickname;
 
     private String profileImage;
