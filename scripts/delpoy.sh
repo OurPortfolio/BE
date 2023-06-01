7b345e60472b57ad3f98cb5e95c2a33d124c4926
park zihoon <bbakzi417@gmail.com>
2023-06-01 19:43:33 +0900

    Fix : ci/cd develop branch 취소

--- scripts/delpoy.sh
+++ scripts/delpoy.sh
@@ -1,7 +1,7 @@
 REPOSITORY=/home/ubuntu/firstcicd
 cd $REPOSITORY
 
-APP_NAME=firstcicd
+APP_NAME=firstcicd-ec2
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
