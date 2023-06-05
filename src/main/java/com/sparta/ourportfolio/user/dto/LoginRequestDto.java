98f71537b925fe8bae7c3d2507514ba3f3eddee2
bakzihun <128371819+bbakzi@users.noreply.github.com>
2023-06-05 18:42:57 +0900

    Update LoginRequestDto.java

--- src/main/java/com/sparta/ourportfolio/user/dto/LoginRequestDto.java
+++ src/main/java/com/sparta/ourportfolio/user/dto/LoginRequestDto.java
@@ -1,9 +1,18 @@
 package com.sparta.ourportfolio.user.dto;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class LoginRequestDto {
     private String email;
     private String password;
+
+    @Builder
+    public LoginRequestDto(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
 }
