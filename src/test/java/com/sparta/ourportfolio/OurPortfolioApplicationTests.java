3e16d14773ccbc2c681562b102a3353a24c9136d
JH4 <spainclub@naver.com>
2023-06-16 02:38:36 +0900

    Refactor : sonarQube 적용 후 코드 수정 ver.3

--- src/test/java/com/sparta/ourportfolio/OurPortfolioApplicationTests.java
+++ src/test/java/com/sparta/ourportfolio/OurPortfolioApplicationTests.java
@@ -1,6 +1,5 @@
 package com.sparta.ourportfolio;
 
-import com.sparta.ourportfolio.user.dto.SignupRequestDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,9 +8,6 @@ class OurPortfolioApplicationTests {
 
     @Test
     void contextLoads() {
-        SignupRequestDto signupRequestDto1 = new SignupRequestDto("test1234@example.com", "test1234", "test1234", null);
-        SignupRequestDto signupRequestDto2 = new SignupRequestDto("test1234@example.com", "test1234", "test1234", null);
-        assert signupRequestDto1.equals(signupRequestDto2);
     }
 
 }
