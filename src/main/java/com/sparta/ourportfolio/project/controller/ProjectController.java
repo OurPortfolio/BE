f4ed73d4a0c9fc8e277ede0df58256680ef67ebb
cozy <wonje1108@gmail.com>
2023-06-17 11:45:13 +0900

    Fix : cors에러 해결 시도 - 2

--- src/main/java/com/sparta/ourportfolio/project/controller/ProjectController.java
+++ src/main/java/com/sparta/ourportfolio/project/controller/ProjectController.java
@@ -14,7 +14,6 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
-@CrossOrigin(origins = "https://ppol.pro", exposedHeaders = "Authorization")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/projects")
